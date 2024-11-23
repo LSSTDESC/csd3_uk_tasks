a0a4b33d6a5c1d86925d2cb0875d8efd41ec9e88
James Chiang <jchiang@slac.stanford.edu>
2024-11-23 06:09:31 +0000

    use absolute path for repo

--- scripts/repo_export/import_registry_data.py
+++ scripts/repo_export/import_registry_data.py
@@ -5,7 +5,8 @@ import argparse
 import lsst.daf.butler as daf_butler
 
 parser = argparse.ArgumentParser()
-parser.add_argument("repo", type=str, help="Destination data repo")
+parser.add_argument("repo_path", type=str,
+                    help="Absolute path to destination data repo")
 parser.add_argument("--export_file", type=str,
                     help="exported yaml file with registry info")
 parser.add_argument("--record_validation_info", action="store_true",
@@ -16,9 +17,9 @@ args = parser.parse_args()
 export_file = os.path.abspath(args.export_file)
 assert os.path.isfile(export_file)
 
-butler = daf_butler.Butler(args.repo, writeable=True)
+butler = daf_butler.Butler(args.repo_path, writeable=True)
 
-butler.import_(directory=repo,
+butler.import_(directory=args.repo_path,
                filename=export_file,
 	       transfer=None,
 	       record_validation_info=args.record_validation_info)
